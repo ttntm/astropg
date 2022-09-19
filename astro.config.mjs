aab6b8bb81bc2d088a66cde2a977b3dfad35b141
ttntm <ttntm@pm.me>
2022-09-19 23:22:01 +0200

    update: sb current approach

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 import { defineConfig } from 'astro/config'
-
+import { loadEnv } from 'vite'
+import storyblok from '@storyblok/astro'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
+
+process.env = { ...process.env, ...loadEnv(process.env.MODE, process.cwd(), '') }
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    storyblok({
+      accessToken: process.env.SB_TOKEN,
+      bridge: false,
+      components: {
+        page: 'components/Page',
+        feature: 'components/storyblok/Feature',
+        grid: 'components/storyblok/Grid',
+        post: 'components/storyblok/Post',
+        teaser: 'components/storyblok/Teaser'
+      }
+    }),
     tailwind(),
     vue()
   ]
